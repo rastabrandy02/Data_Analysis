7aa12e95e9a399cd08a3eb31afbbe66058dccc6d
Carlos <arnau.carlos01@gmail.com>
2024-01-09 13:09:47 +0100

    Changed death php

--- php/death.php
+++ php/death.php
@@ -4,12 +4,20 @@ include_once 'settings.php';
 
 if(true)
 {    
-     $Xpos = $_GET['Xpos']; 
-     $Ypos = $_GET['Ypos'];
-     $Zpos = $_GET['Zpos'];
+   $Xpos = $_GET['Xpos']; 
+   $Ypos = $_GET['Ypos'];
+   $Zpos = $_GET['Zpos'];
 
-     $sql = "INSERT INTO Death (Xpos,Ypos,Zpos)
-     VALUES ('$Xpos' ,'$Ypos','$Zpos')";
+   $Damager=$_GET['Damager'];
+
+   $Amount=$_GET['Amount'];
+
+   $XDmgSource=$_GET['XDmgSource'];
+   $YDmgSource=$_GET['YDmgSource'];
+   $ZDmgSource=$_GET['ZDmgSource'];
+
+   $sql = "INSERT INTO Death (Xpos,Ypos,Zpos,Damager,Amount,XDmgSource,YDmgSource,ZDmgSource,)
+   VALUES ('$Xpos','$Ypos','$Zpos','$Damager','$Amount','$XDmgSource','$YDmgSource','$ZDmgSource')";
      if (mysqli_query($conn, $sql)) {
         echo "New record has been added successfully! ";
 
